f745a36ac3bc5e2eb9814350603ee249b941587d
Monlih <MonliH@users.noreply.github.com>
2020-04-29 11:18:29 -0400

    [CI] Removed benchmarks (for the time being)

--- benches/parser_bench.rs
+++
@@ -1,9 +0,0 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use lib::parser::parser::Parser;
-
-pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("parser test", |b| b.iter(||  Parser::new(black_box("tests/parser_test.fluo".to_string())).unwrap().parse()));
-}
-
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
