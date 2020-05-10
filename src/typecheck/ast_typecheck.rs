442a0c4236ac0437e920c84c6fb9dc4fe243ecd2
Monlih <MonliH@users.noreply.github.com>
2020-05-10 14:23:03 -0400

    [Typecheck] Added panic on varaible assign and declaration

--- src/typecheck/ast_typecheck.rs
+++ src/typecheck/ast_typecheck.rs
@@ -500,6 +500,14 @@ impl<'a> TypeCheckType<'a> {
                     cast_type
                 }
             }
+            Expr::VariableAssign(variable_assign) => {
+                // TODO: add variable assign to type checker
+                panic!("Inference for variable assign not implemented yet");
+            }
+            Expr::VariableDeclaration(variable_assign) => {
+                // TODO: add variable declaration to type checker
+                panic!("Inference for variable assign not implemented yet");
+            }
             Expr::Infix(infix) => {
                 let left_type = TypeCheckType::from_expr(&*infix.left, context)?;
                 let right_type = TypeCheckType::from_expr(&*infix.right, context)?;
