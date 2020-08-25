272b59bfaf1f7836511b96e88f2aa27218320c94
Jonathan <30177086+MonliH@users.noreply.github.com>
2020-08-24 21:41:51 -0400

    [Tests] Fix tests and benchmarks

--- benches/parser_bench.rs
+++ benches/parser_bench.rs
@@ -1,20 +1,19 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use lib::logger;
+use criterion::{criterion_group, criterion_main, Criterion};
+use lib::logger::LoggerInner;
 use lib::parser;
+use lib::sourcemap::SourceMapInner;
 
-use std::cell::RefCell;
 use std::path;
 use std::rc::Rc;
 
 #[allow(unused_must_use)]
 fn criterion_benchmark(c: &mut Criterion) {
-    let logger = Rc::new(RefCell::new(logger::logger::Logger::new(true)));
-
-    c.bench_function("parser simple", |b| {
-        b.iter(|| {
-            let mut my_parser = parser::parser::Parser::new(
-                black_box(path::Path::new("my_long_filename_test_this_is_really_long_but_its_for_a_Test_so_who_cares.fl")),
-                black_box(r#"/*	
+    let sourcemap = SourceMapInner::new();
+    sourcemap.borrow_mut().insert_file(
+        path::PathBuf::from(
+            "my_long_filename_test_this_is_really_long_but_its_for_a_Test_so_who_cares.fl",
+        ),
+        r#"/*	
 hlleoa1234567890qwertyuiopasdfghjklzxcvbnm,./?><;'":[]\|}{=-0987654321`~!@#$%^&*()_+	
 one two	
 djawd	
@@ -34,15 +33,28 @@ def other() {
     let _qwertyuiopasdfghjklzxcvbnm: int;	
     _qwertyuiopasdfghjklzxcvbnm = "hi";	
     hi(10, 102, _qwertyuiopasdfghjklzxcvbnm+"other hi", x);	
+    return (((((((1023), (((123))), x = 10, (((let x: int = 10))), (("awd"))))))));
+    (((((((((((((())))))))))))));
 }
-"#),
-                Rc::clone(&logger),
-            );
+"#
+        .to_string(),
+    );
+
+    let logger = LoggerInner::new(true, Rc::clone(&sourcemap));
+
+    c.bench_function("parser simple", |b| {
+        b.iter(|| {
+            let mut my_parser = parser::parser::Parser::new(0, Rc::clone(&logger), Rc::clone(&sourcemap));
             my_parser.initialize_expr();
             my_parser.parse();
         })
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!{
+    name = benches;
+    config = Criterion::default().sample_size(10);
+    targets = criterion_benchmark
+}
+
 criterion_main!(benches);
